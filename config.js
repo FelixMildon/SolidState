e366e5ea163e6eb72d03d89b7f5ce8c71c223e97
FelixMildon <felixmildon@gmail.com>
2020-10-14 10:09:05 +1300

    test

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: 'NetGala',
+  subHeading: 'Web master in charge of increasing sales and conversion',
   // social
   socialLinks: [
     {
