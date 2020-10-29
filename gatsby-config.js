403d4a192f46f5aec64571ea7a36fa429919e8fc
FelixMildon <felixmildon@gmail.com>
2020-10-29 19:25:58 +1300

    adding gtag.js plugin and npm install

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,18 @@ module.exports = {
         display: config.manifestDisplay,
         icon: config.manifestIcon, // This path is relative to the root of the site.
       },
-    },
+      
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `G-0WH8RN97FB`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+        },
+      },
+    
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
