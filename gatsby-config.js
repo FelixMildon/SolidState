e95dd137eb543644a45d16d4c5ef4765372ad6e4
FelixMildon <felixmildon@gmail.com>
2020-10-30 16:10:35 +1300

    adding sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
     title: config.siteTitle,
+    siteUrl: `https://www.netgala.co.nz`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -32,5 +33,6 @@ module.exports = {
     
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    'gatsby-plugin-sitemap',
   ],
 };
