55f069f95adbe9ca3e1604d5c4bcc9173656cd01
FelixMildon <felixmildon@gmail.com>
2020-12-17 17:42:52 +1300

    deleting a rouge plus in the codebase lol

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
         </p>
         <form className="contact-form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="bot-field" />
-+       <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="form-name" value="contact" />
             <div className="field">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
